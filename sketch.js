4ea565eeca5da4507f75646f044017618dd2ebdc
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-05-03 17:03:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var fruits, fLen, i;
+fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+function setup() {
+  
+  fLen = fruits.length;
+
+  for (i = 0; i < fLen; i++) {
+  console.log( fruits[i]);
+  }}
+
+function draw() {
+  background(220);
+}
